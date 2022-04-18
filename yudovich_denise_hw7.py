ae44fa0f563aa907cb302d11f02f91e28a2b10d5
Denise Yudovich <deniseyudovich@c0709a-s30.ufhpc>
2022-04-18 17:44:47 -0400

    m

--- yudovich_denise_hw7.py
+++ yudovich_denise_hw7.py
@@ -7,19 +7,19 @@ import matplotlib.pyplot as plt
 #read sed.txt and create array from data
 text_file = open("sed.txt", "r")
 sed = np.loadtxt('sed.txt', delimiter = ",")
-#print(sed)
-wave = sed[:, 0]     #*u.micron
-lum = sed[:, 1]     #*(u.Lsun/u.micron)
+wave = sed[:, 0]    
+lum = sed[:, 1]     
 print("wave: ", wave)
 print("lum: ", lum)
 
 
 #Create new array of x (wavelength) in specified distribution
 b= np.where((wave<=1000) & (wave>=10))
-
 newWave = np.array(wave[b])
+#Create new arrat of y (luminosities) in the same index positions found from np.where()
 newlum = np.array(lum[b])
 output = np.trapz(newlum, -newWave)
+#Convert units
 output *= u.Lsun
 output2 = output.to(u.erg/u.s)
 print("output2: ", output2)
